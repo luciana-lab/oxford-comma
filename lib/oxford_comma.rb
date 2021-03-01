aafe68943b115913ae28afecd27695cd4a5b7662
Luciana Sugeta <lulusugeta@gmail.com>
2021-03-01 14:43:38 -0500

    commit tree b3578282860bf86cd8eac998a06d44a9a9d4ce83

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
-end
+    if  array.length == 2
+        array.join(" and ")
+    elsif array.length > 2
+        [array[0..-2]].join(", ") << ", and " << array.last
+    else
+        array.join
+    end
+    end
